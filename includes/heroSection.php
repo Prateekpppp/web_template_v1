a68d23a3091dc857e52af88428b66c125110ab1f
Prateekpppp <prateekpppp4gmail.com>
2026-04-09 23:38:54 +0530

    up

--- includes/heroSection.php
+++ includes/heroSection.php
@@ -1,6 +1,35 @@
 
       <!-- ========================= hero-2 start ========================= -->
-      <div class="hero-section hero-style-2">
+        <div class="hero-section position-relative d-flex overflow-hidden justify-content-center">
+          <video class="" autoplay muted loop id="myVideo">
+            <source src="assets/video/nut.mp4" type="video/mp4">
+          </video>
+          <div class="position-absolute z-index-5 top-0 left-0 w-100 h-100 d-flex align-items-center">
+            <div class="container">
+              <div class="row align-items-end">
+                <div class="col-lg-12">
+                  <div class="hero-content-wrapper">
+                    <h4 class="wow fadeInUp d-none" data-wow-delay=".2s">You're Using</h4>
+                    <h2 class="mb-1 wow fadeInUp" data-wow-delay=".4s">Nuts &</h2>
+                    <h2 class="mb-0 wow fadeInUp" data-wow-delay=".4s">Bolts</h2>
+                    <p class="mb-50 mb-sm-10 wow fadeInUp app_color_green text-capitalize" data-wow-delay=".6s">of any size</p>
+                    <div class="d-flex justify-content-evenly mt-50 pt-50">
+                        <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Catalogue</a>
+                        <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Get Quote</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 d-none">
+                  <div class="hero-image">
+                    <img src="assets/img/hero/hero-2/hero-img.svg" alt="" class="wow fadeInRight" data-wow-delay=".2s">
+                    <img src="assets/img/hero/hero-2/paattern.svg" alt="" class="shape shape-1">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <!-- <div class="hero-section hero-style-2">
         <div class="container">
           <div class="row align-items-end">
             <div class="col-lg-12">
@@ -26,5 +55,5 @@
         <video autoplay muted loop id="myVideo">
           <source src="assets/video/nut.mp4" type="video/mp4">
         </video>
-      </div>
+      </div> -->
       <!-- ========================= hero-2 end ========================= -->
---
+++ includes/heroSection1.php
@@ -0,0 +1,30 @@
+
+      <!-- ========================= hero-2 start ========================= -->
+      <div class="hero-section hero-style-2">
+        <div class="container">
+          <div class="row align-items-end">
+            <div class="col-lg-12">
+              <div class="hero-content-wrapper">
+                <h4 class="wow fadeInUp d-none" data-wow-delay=".2s">You're Using</h4>
+                <h2 class="mb-1 wow fadeInUp" data-wow-delay=".4s">Nuts &</h2>
+                <h2 class="mb-0 wow fadeInUp" data-wow-delay=".4s">Bolts</h2>
+                <p class="mb-50 mb-sm-10 wow fadeInUp app_color_green text-capitalize" data-wow-delay=".6s">of any size</p>
+                <div class="d-flex justify-content-evenly">
+                    <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Catalogue</a>
+                    <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Get Quote</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 d-none">
+              <div class="hero-image">
+                <img src="assets/img/hero/hero-2/hero-img.svg" alt="" class="wow fadeInRight" data-wow-delay=".2s">
+                <img src="assets/img/hero/hero-2/paattern.svg" alt="" class="shape shape-1">
+              </div>
+            </div>
+          </div>
+        </div>
+        <video autoplay muted loop id="myVideo">
+          <source src="assets/video/nut.mp4" type="video/mp4">
+        </video>
+      </div>
+      <!-- ========================= hero-2 end ========================= -->
