059762fed80b82df7df85efb25b1ccbda20cacce
Prateekpppp <prateekpppp4gmail.com>
2026-04-11 07:48:56 +0530

    up

--- includes/heroSection.php
+++ includes/heroSection.php
@@ -1,6 +1,6 @@
 
       <!-- ========================= hero-2 start ========================= -->
-        <div class="hero-section position-relative d-flex overflow-hidden justify-content-center">
+        <div class="videoBanner hero-section position-relative d-flex overflow-hidden justify-content-center">
           <video class="" autoplay muted loop id="myVideo">
             <source src="assets/video/nut.mp4" type="video/mp4">
           </video>
@@ -8,14 +8,15 @@
             <div class="container">
               <div class="row align-items-end">
                 <div class="col-lg-12">
-                  <div class="hero-content-wrapper">
+                  <div class="hero-content-wrapper position-relative z-1">
+                    <div class="yellow_circle"></div>
                     <h4 class="wow fadeInUp d-none" data-wow-delay=".2s">You're Using</h4>
-                    <h2 class="mb-1 wow fadeInUp" data-wow-delay=".4s">Nuts &</h2>
+                    <h2 class="mb-1 wow fadeInUp fs-2" data-wow-delay=".4s">Nuts &</h2>
                     <h2 class="mb-0 wow fadeInUp" data-wow-delay=".4s">Bolts</h2>
-                    <p class="mb-50 mb-sm-10 wow fadeInUp app_color_green text-capitalize" data-wow-delay=".6s">of any size</p>
+                    <p class="mb-50 mb-sm-10 wow fadeInUp app_color_green text-capitalize fw-bold" data-wow-delay=".6s">of any size</p>
                     <div class="d-flex justify-content-evenly mt-50 pt-50">
-                        <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Catalogue</a>
-                        <a href="#" rel="nofollow" target="blank" class="button button-sm radius-10 wow fadeInUp" data-wow-delay=".7s">Get Quote</a>
+                        <a href="#" rel="nofollow" target="blank" class="button button-lg radius-10 wow fadeInUp" data-wow-delay=".7s">Catalogue</a>
+                        <a href="#" rel="nofollow" target="blank" class="button button-lg radius-10 wow fadeInUp" data-wow-delay=".7s">Get Quote</a>
                     </div>
                   </div>
                 </div>
--- includes/productSection.php
+++ includes/productSection.php
@@ -1,8 +1,43 @@
 
     <!-- ========================= feature style-2 start ========================= -->
+    
+    <style>
+        
+@media (max-width: 991px) {
+  #myVideo {
+    width: auto;
+    /* margin: 11.6483px 0px 0px -447.937px;
+    transform: translate3d(0px, -11.6483px, 0px); */
+  }
+
+  /* .hover_item {
+    position: unset;
+  } */
+
+  .hover-popup {
+    position: fixed !important;
+    left: 0 !important;
+    top: 20% !important;
+    width: 90% !important;
+    transform-origin: unset;
+  }
+
+  .hover-popup::before {
+    content: '';
+    position: absolute;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid #dbd9d9;
+    left: 50%;
+    transform: rotate(0deg);
+    top: -10px;
+  }
+}
+    </style>
+    
     <section id="services" class="feature-section feature-style-2">
       <div class="container">
-            <div class="d-flex flex-column justify-content-center">
+            <div class="d-flex flex-md-column flex-row flex-wrap my-5 justify-content-center">
                 <div class="col-md-3 col-sm-6 p-2 position-relative">
                     <div class="single-team wow fadeInUp" data-wow-delay=".2s">
                         <div class="image w-fit mx-auto hover_item hover-target">
